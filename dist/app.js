27216abdc626bc52076e892f7631134f960b7aa4
kawa1228 <sxa.1105bo@gmail.com>
2018-09-05 16:29:03 +0900

    first

---
+++ dist/app.js
@@ -0,0 +1,144 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+const dat = new Date
+
+getCalendarData = (year, month) => {
+    // 選択した月の初日の情報
+    const firstDate = new Date(year,(month -1),1)
+    // 初日の曜日
+    const weekday = firstDate.getDay()
+
+    // 末日
+    const lastDay = new Date(firstDate.getFullYear(),firstDate.getMonth()+1,0).getDate()
+    // カレンダー情報
+    const calendarData = []
+    let weekdayCount = weekday
+    for (let i = 0; i < lastDay; i++) {
+        calendarData[i] = {
+            day: i + 1,
+            weekday: weekdayCount
+        }
+        // 曜日のカウントが6(土曜日)まできたら0(日曜日)に戻す
+        if(weekdayCount >= 6) {
+            weekdayCount = 0
+        } else {
+            weekdayCount += 1
+        }
+    }
+    return calendarData
+}
+
+const year = 2018
+const month = 8
+console.log(getCalendarData(year, month))
+
+//今日の年月日を取得
+class GetYMD {
+    getYear() {
+        return dat.getFullYear()
+    }
+    getMonth() {
+        return dat.getMonth()
+    }
+    getDate() {
+        return dat.getDate()
+    }
+    getWeek() {
+        const week = ['日','月','火','水','木','金','土']
+        return week[dat.getDay()]
+    }
+}
+
+const getYMD = new GetYMD()
+// console.log(getYMD.getWeek())
+
+
+const titleYear = document.getElementById('title-year')
+const titleMonth = document.getElementById('title-month')
+
+//HTMLに出力
+class OutPut {
+    year() {
+        titleYear.textContent = `${getYMD.getYear()}年`
+    }
+    month() {
+        titleMonth.textContent = `${getYMD.getMonth()+1}月`
+    }
+}
+
+const outPut = new OutPut()
+outPut.year()
+outPut.month()
+
+
+
+
+/***/ })
+/******/ ]);

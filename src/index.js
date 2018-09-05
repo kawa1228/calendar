d38e86602c8f53329acf0c8b6a59093279039f3b
kawa1228 <sxa.1105bo@gmail.com>
2018-09-05 16:49:09 +0900

    class構文へ書き換え

--- src/index.js
+++ src/index.js
@@ -1,36 +1,39 @@
-const dat = new Date
-
-getCalendarData = (year, month) => {
-    // 選択した月の初日の情報
-    const firstDate = new Date(year,(month -1),1)
-    // 初日の曜日
-    const weekday = firstDate.getDay()
-
-    // 末日
-    const lastDay = new Date(firstDate.getFullYear(),firstDate.getMonth()+1,0).getDate()
-    // カレンダー情報
-    const calendarData = []
-    let weekdayCount = weekday
-    for (let i = 0; i < lastDay; i++) {
-        calendarData[i] = {
-            day: i + 1,
-            weekday: weekdayCount
-        }
-        // 曜日のカウントが6(土曜日)まできたら0(日曜日)に戻す
-        if(weekdayCount >= 6) {
-            weekdayCount = 0
-        } else {
-            weekdayCount += 1
-        }
-    }
-    return calendarData
-}
-
 const year = 2018
 const month = 8
-console.log(getCalendarData(year, month))
+
+class CalendarData {
+    getData(year, month) {
+        // 初日の情報
+        const firstDate = new Date(year,(month -1),1)
+        // 初日の曜日
+        const weekday = firstDate.getDay()
+        // 末日
+        const lastDay = new Date(firstDate.getFullYear(),firstDate.getMonth()+1,0).getDate()
+        // カレンダー情報
+        const calendarData = []
+        let weekdayCount = weekday
+        for (let i = 0; i < lastDay; i++) {
+            calendarData[i] = {
+                day: i + 1,
+                weekday: weekdayCount
+            }
+            // 曜日のカウントが6(土曜日)まできたら0(日曜日)に戻す
+            if(weekdayCount >= 6) {
+                weekdayCount = 0
+            } else {
+                weekdayCount += 1
+            }
+        }
+        return calendarData
+    }
+}
+
+const calendarData = new CalendarData()
+console.log(calendarData.getData(year, month))
 
 //今日の年月日を取得
+const dat = new Date()
+
 class GetYMD {
     getYear() {
         return dat.getFullYear()
